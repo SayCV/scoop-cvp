092aadf4a5805575a7525ad8585b087a73e26ee1
SayCV <sssxyc@163.com>
2022-03-12 17:04:59 +0800

    synopsys-vcs201712-wsl-env: update to version 2017.12

--- scripts/synopsys-vcs201712-wsl/synopsys-installer35-wsl-install.sh
+++ scripts/synopsys-vcs201712-wsl/synopsys-installer35-wsl-install.sh
@@ -16,7 +16,7 @@ installer_version=v3.5
 
 # 'cshell' is required to run the installer
 sudo apt-get install -y --no-install-recommends csh
-sudo apt-get install -y lsb-core bc libjpeg62-dev
+sudo apt-get install -y lsb-core bc libjpeg62-dev libpng12-dev
 
 wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb \
   && sudo dpkg -i /tmp/libpng12.deb \
