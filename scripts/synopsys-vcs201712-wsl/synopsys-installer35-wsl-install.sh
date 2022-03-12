7c9ce2defbcb3584515abd14b76b6d9e163c2ef5
SayCV <sssxyc@163.com>
2022-03-12 10:48:56 +0800

    synopsys-verdi201606-wsl: update to version 2016.06

--- scripts/synopsys-vcs201712-wsl/synopsys-installer35-wsl-install.sh
+++ scripts/synopsys-vcs201712-wsl/synopsys-installer35-wsl-install.sh
@@ -16,7 +16,11 @@ installer_version=v3.5
 
 # 'cshell' is required to run the installer
 sudo apt-get install -y --no-install-recommends csh
-sudo apt-get install -y lsb-core bc
+sudo apt-get install -y lsb-core bc libjpeg62-dev
+
+wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb \
+  && sudo dpkg -i /tmp/libpng12.deb \
+  && rm /tmp/libpng12.deb
 
 [ -d /usr/tmp ] || sudo mkdir /usr/tmp
 [ -f /usr/tmp/.flexlm ] || sudo touch /usr/tmp/.flexlm
